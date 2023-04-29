434dcc486e5101662ab41e66d5b2c2667b887cb0
Yu <galichy79@gmail.com>
2023-04-29 10:48:05 +0300

    Init database, make @db

--- app.rb
+++ app.rb
@@ -5,6 +5,16 @@ require 'sinatra/reloader'
 require 'bundler/setup'
 require 'sqlite3'
 
+def init_db 
+  @db = SQLite3::Database.new 'leprosorium.db'
+  @db.results_as_hash = true
+end
+
+
+before do 
+  init_db
+end
+
 get '/' do 
   erb 'Hello'
 end
