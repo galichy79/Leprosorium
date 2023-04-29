c4247fc55ade66d917e59e3c4dfc296edd1a7132
Yu <galichy79@gmail.com>
2023-04-29 15:02:18 +0300

    Add /details/...handler

--- app.rb
+++ app.rb
@@ -52,3 +52,11 @@ post '/new' do
   
   
 end
+
+# вывод информации о посте 
+
+get '/details/:post_id' do 
+  post_id = params[:post_id]
+
+  erb "Displaying information for post with id #{post_id}"
+end
